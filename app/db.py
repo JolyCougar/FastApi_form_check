efd2d6ede5403845fafc4843b4be41458bb62d57
Yuri <Jolycougar@gmail.com>
2024-12-08 18:24:57 +0300

    add: take templates from db

--- app/db.py
+++ app/db.py
@@ -1,13 +1,40 @@
 import json
-from pymongo import MongoClient
+from pymongo import MongoClient, errors
 
 
 def get_db():
-    client = MongoClient("mongodb://mongo:27017/")
-    db = client["forms_db"]
-    return db
+    try:
+        client = MongoClient("mongodb://localhost:27017/")
+        db = client["forms_db"]
+        return db
+    except errors.ConnectionFailure as e:
+        print(f"Ошибка подключения к базе данных MongoDB: {e}")
+        raise
+
+
+def insert_templates_to_db():
+    db = get_db()
+    templates_collection = db["templates"]
+    with open("app/templates.json", "r") as f:
+        templates_data = json.load(f)
+    try:
+        templates_collection.insert_many(templates_data, ordered=False)
+        print("Шаблоны успешно добавлены в базу данных.")
+    except errors.BulkWriteError as e:
+        print(f"Ошибка при добавлении данных в базу данных: {e}")
+
+    return templates_data
 
 
 def get_templates():
-    with open("app/templates.json", "r") as f:
-        return json.load(f)
+    db = get_db()
+    templates_collection = db["templates"]
+
+    templates = list(templates_collection.find())
+    if templates:
+        return templates
+    else:
+        insert_templates_to_db()
+
+
+
